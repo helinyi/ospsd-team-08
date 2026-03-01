f35cba8b0cd8cbbcb7dd3873c553b41c3d1dcadd
calico-w <jw8221@nyu.edu>
2026-03-01 17:20:03 -0500

    Update test_di_registration.py

--- components/chat_client_api/tests/test_di_registration.py
+++ components/chat_client_api/tests/test_di_registration.py
@@ -1,1 +1,22 @@
+# components/chat_client_api/tests/test_di_registration.py
 
+import pytest
+from chat_client_api import get_client, register_client_factory
+
+
+class DummyClient:
+    pass
+
+
+def test_get_client_raises_when_unregistered():
+    with pytest.raises(RuntimeError):
+        get_client()
+
+
+def test_register_client_factory():
+    def factory():
+        return DummyClient()
+
+    register_client_factory(factory)
+    client = get_client()
+    assert isinstance(client, DummyClient)
